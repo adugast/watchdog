5a6465a17185c9de493d7c7a85f64b0ada518ad4
pestbuns <ant.one.dugast@gmail.com>
2017-12-09 15:07:03 +0100

    add documentation on the watchdog system

--- src/main.c
+++ src/main.c
@@ -9,13 +9,39 @@
 
 /////////////////////////////////////////////////////////////////////
 /*!
- *      == W A T C H D O G ==
+ *      == L I N U X   W A T C H D O G   T I M E R ==
  *
- * DESCRIPTION:
+ * The linux watchdog timer allows the linux kernel to
+ * reset the system if serious problems are detected.
+ *
+ * It's composed of three different layers:
+ *      1) watchdog hardware (hardware)
+ *      2) watchdog driver (kernel)
+ *      3) watchdog daemon (userspace)
+ *
+ * The watchdog driver makes available in the filesystem the corresponding
+ * device file to make the link between the hardware and userspace.
+ * By default, it's located in "/dev/watchdog".
+ *
+ * watchdogd:
+ *      - The watchdog daemon opens /dev/watchdog.
+ *
+ *      - It keeps writing to the device file often enough to
+ *      prevent the kernel from resetting. (at least once per minute).
+ *      Each write delays the reboot time another minute.
+ *
+ *      - If a system issue occurs (process table full, not enough free memory,
+ *      temperature too high, file table overflow, ...) describes in man (8).
+ *      The watchdog the daemon will stop writing to the device file.
+ *
+ *      - After a minute of inactivity the watchdog hardware will cause the reset.
+ *
+ * PROGRAM DESCRIPTION:
  *      Show hardware watchdog status. The default device is /dev/watchdog.
  *      Note that the number of supported watchdog features is hardware specific.
  *
- * INFO:
+ * MORE INFO:
+ *      https://en.wikipedia.org/wiki/Watchdog_timer
  *      https://www.kernel.org/doc/Documentation/watchdog/watchdog-api.txt
  *      man (8) wdctl
  *
